3af5565a649d4ec2dc08c68be5abb15194dca914
tomvleeuwen <github@tomvanleeuwen.nl>
2017-12-10 12:43:36 +0100

    Update logfiles with the new information and add testcase for bugfix in commit a8cff8

--- ci/run_ci.sh
+++ ci/run_ci.sh
@@ -10,7 +10,7 @@ mkdir -p results/eee
 # Just ignore all lines that start with at least two spaces
 for file in $(ls input/dflash/) ; do
     echo "Converting ${file}"
-    ../dflash_to_eee.py input/dflash/${file} results/eee/${file} 2>&1 | grep --text --invert-match "^  " > results/logs/${file}
+    ../dflash_to_eee.py input/dflash/${file} results/eee/${file} 2>&1 | grep --text --invert-match "^  File" > results/logs/${file}
 done
 
 # Since we have set -e , a diff will automatically exit the script with an error.
