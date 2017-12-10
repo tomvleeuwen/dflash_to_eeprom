79631731916db27c906af39e361db1eb94b12061
tomvleeuwen <github@tomvanleeuwen.nl>
2017-12-10 13:34:47 +0100

    Fix build.cmd

--- build.cmd
+++ build.cmd
@@ -1,4 +1,5 @@
 @echo off
 REM Simply call pyinstaller and rename the output file
-C:\Python2.7\Scripts\pyinstaller.exe gui.py --onefile
+C:\Python27\Scripts\pyinstaller.exe gui.py --onefile
 move dist\gui.exe dflash_to_eee.exe
+rmdir dist
